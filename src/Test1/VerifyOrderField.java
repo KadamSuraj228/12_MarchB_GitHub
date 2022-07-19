3835bce1f7908ca60d6d51ab2a7c656f6e60ad9c
BusinessComputers.in <BusinessComputers.in@DESKTOP-V2PIM7G>
2022-07-19 23:34:53 +0530

    [Suraj]: added new Package_Test3

--- src/Test1/VerifyOrderField.java
+++ src/Test1/VerifyOrderField.java
@@ -1,5 +1,12 @@
 package Test1;
 
-public class VerifyOrderField {
+public class VerifyOrderField 
+{
+	public static void main(String[] args) 
+	{
+		System.out.println("Test case pass, Orders field is present");
+		
+		System.out.println("Test case fail, Orders field is not Present");
+	}
 
 }
---
+++ src/Test3/VerifyProfileField.java
@@ -0,0 +1,11 @@
+package Test3;
+
+public class VerifyProfileField 
+{
+	public static void main(String[] args)
+	{
+		System.out.println("Test case pass, Profile field is Displayed");
+		
+	}
+
+}
